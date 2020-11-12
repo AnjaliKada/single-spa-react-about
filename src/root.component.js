27c620d5699409286123c22d2232212c097145ff
AnjaliKada <52856473+AnjaliKada@users.noreply.github.com>
2020-11-12 18:03:57 +0530

    Add files via upload

---
+++ src/root.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./root.component.css";
+
+export default function Root() {
+  return (
+    <div className="container2">
+      <p>About App</p>
+    </div>
+  );
+}
